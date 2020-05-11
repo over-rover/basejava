486e90688facdcb6bde62049decc7eaff1c62cd3
MyName Ok <gh.pueblo@gm.com>
2020-05-11 14:12:01 +0300

    Update ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+
     void clear() {
+        for(int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = 0;
+        while (!storage[i].uuid.equals(uuid) && i < size()) {
+            i++;
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (!storage[i].uuid.equals(uuid) && i < size()) {
+            i++;
+        }
+        storage[i] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while(storage[i] != null) {
+            i++;
+        }
+        return i;
     }
 }
