0acee939055a6cd44a03c06a678cbf062e83b404
MyName Ok <gh.pueblo@gm.com>
2020-05-11 14:14:48 +0300

    some comments in MainTestArrayStorage

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -19,11 +19,11 @@ public class MainTestArrayStorage {
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        //System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy")); // Некорреткный ввод. Пока непонятно как нужно реагировать
 
         printAll();
-        ARRAY_STORAGE.delete(r1.uuid);
-        printAll();
+        //ARRAY_STORAGE.delete(r1.uuid); // делает дырку в массиве, хотя в условии сказано, что массив не разбавлен null
+        //printAll();
         ARRAY_STORAGE.clear();
         printAll();
 
