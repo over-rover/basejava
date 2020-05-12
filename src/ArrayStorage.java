cdeb9e3c4f295e643b51d3077282921aa02a09ec
MyName Ok <gh.pueblo@gm.com>
2020-05-12 16:58:51 +0300

    Update ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,46 +5,59 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size; // полезный размер массива
 
 
     void clear() {
-        for(int i = 0; i < size(); i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        // Запись while (!storage[i].uuid.equals(uuid) && storage[i] != null) приводила к исключению при достижении
+        // null, так как значение строки сравнивалось с пустотой. Поменял местами условия
         int i = 0;
-        while (!storage[i].uuid.equals(uuid) && i < size()) {
+        while (storage[i] != null && !storage[i].uuid.equals(uuid)) {
             i++;
         }
         return storage[i];
+
+        /* // Резервный способ. Пока мучился с while нарисовался этот вариант
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
+        return null;*/
     }
 
     void delete(String uuid) {
         int i = 0;
-        while (!storage[i].uuid.equals(uuid) && i < size()) {
+        while (storage[i] != null && !uuid.equals(storage[i].uuid)) {
             i++;
         }
-        storage[i] = null;
+        // при выходе из цикла индекс указывает или на позицию искомого uuid или позицию первого null
+        if (i < size) {
+            storage[i] = storage[size-1];
+            storage[size-1] = null;
+            size--;
+        } else {
+            System.out.println("Указанное значение не найдено. Проверьте правильность ввода");
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
-    int size() {
-        int i = 0;
-        while(storage[i] != null) {
-            i++;
-        }
-        return i;
+    public int size() {
+        return size;
     }
 }
