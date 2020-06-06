cb64bd46eeed4db3b22751161f264217fc575e03
MyName Ok <gh.pueblo@gm.com>
2020-06-06 22:15:33 +0300

    HW3 rel06: introduced getIndex() with self-made binary search, insert(), extract()

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -2,84 +2,46 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 public class SortedArrayStorage extends AbstractArrayStorage {
+    private int insertIndex;
+
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-        int low = 0;
-        int high = size - 1;
-        int step = 1;
+        /*Далее реализован двоичный поиск по аналогии с методом Arrays.binarySearch()
+         * Однако, есть нюанс. При сравнении строк "uuid8" оказывается больше, чем "uuid23"
+         * Возможно из-за отсутствия в коде записи Comparable midVal = (Comparable)a[mid]
+         * По словам Татьяны Arrays.binarySearch() дает лексикографическое сравнение строк
+         * и такой проблемы там нет.*/
+        int low = 0, high = size - 1;
         while (low <= high) {
-            int mid = low + (high - low)/2;
-
+            int mid = low + (high - low) / 2;
             int cmp = searchKey.compareTo(storage[mid]);
-            System.out.println(step +" step (beg): " + "low = " + low + "\t" + "high = " + high + "\t" + "mid = " + mid + "\t" + "cmp = " + cmp);
-
             if (cmp > 0)
                 low = mid + 1;
             else if (cmp < 0)
                 high = mid - 1;
             else
                 return mid; // key found
-            System.out.println(step +" step (end): " + "low = " + low + "\t" + "high = " + high + "\t" + "mid = " + mid + "\t" + "cmp = " + cmp);
-            if (low < high)
-                System.out.println(Arrays.toString(Arrays.copyOfRange(storage, low, high+1)));
-            step++;
         }
-
+        insertIndex = low; // storage[insertIndex] - точка вставки резюме
         return -(low + 1);  // key not found.
-        /*В этом случае нужно сдвигать все элементы, начиная со storage[low], а
-        * на место  storage[low] вставлять новое резюме*/
-
-        //return Arrays.binarySearch(storage, 0, size, searchKey);
-        /*
-         * В binarySearch() сравниваются объекты типа Resume, а у нас на входе String,
-         * поэтому пришлось придумывать костыли и создавать промежуточный объект searchKey и
-         * присваивать ему uuid
-         */
     }
 
     protected void insert(Resume r) {
-        storage[size] = r;
+        for (int i = size; i >= insertIndex; i--) {
+            storage[i + 1] = storage[i];
+            // проверка на возможное переполнение произведена в save().
+        }
+        // IDEA предложила альтернативу, но она вообще не дружественная
+        // System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size + 1 - insertIndex);
+
+        storage[insertIndex] = r;
         size++;
-        /*if (size == 0) {
-            storage[0] = r;
-            System.out.println("Нулевое резюме  " + storage[0].getUuid());
-            size++;
-        } else {
-
-            int low = 0, high = size - 1;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (r.compareTo(storage[mid]) > 0) {
-                    if (r.compareTo(storage[mid + 1]) > 0) {
-                        low = mid + 1;
-                    } else {
-                        // вставляем резюме между storage[mid] и storage[mid+1]
-                        spread(r, mid);
-                    }
-                } else {
-                    if (r.compareTo(storage[mid - 1]) < 0) {
-                        high = mid - 1;
-                    } else {
-                        // вставляем резюме между storage[mid-1] и storage[mid]
-                        spread(r, mid - 1);
-                    }
-                }
-            }
-
-        }*/
     }
 
-    private void spread(Resume r, int index) {
-        size++; // может быть переполнение
-        for (int i = size; i > index; i--) {
-            storage[i] = storage[i - 1];
-        }
-        // IDEA предложила такую альтернативу
-        //if (size - index >= 0) System.arraycopy(storage, index, storage, index + 1, size - index);
-        storage[index + 1] = r;
+    protected void extract(int index) {
+        System.arraycopy(storage, index + 1, storage, index, size);
+        size--;
     }
 }
