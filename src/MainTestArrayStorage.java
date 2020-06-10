84be7e2d997d360c3abf681c8793f231a74fba6e
MyName Ok <gh.pueblo@gm.com>
2020-06-10 19:02:49 +0300

    HW3 rel07: modified printing messages by showing uuid

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -22,35 +22,41 @@ public class MainTestArrayStorage {
 
         final Resume r4 = new Resume();
         r4.setUuid("uuid10");
-        System.out.println("Save " + r4.getUuid());
+        System.out.println("Save r4 " + r4.getUuid() + " result:");
         ARRAY_STORAGE.save(r4);
         printAll();
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get r2: " + ARRAY_STORAGE.get(r2.getUuid()));
+        System.out.println("Get r2 result: " + ARRAY_STORAGE.get(r2.getUuid()));
+        System.out.println("Get r4 result: " + ARRAY_STORAGE.get(r4.getUuid()));
 
-        System.out.println("Обращемся к несуществующему резюме");
+        System.out.println("Обращемся к несуществующему резюме uuid42");
         System.out.println(ARRAY_STORAGE.get("uuid42"));
 
-        System.out.println("Пытаемся удалить несуществующее резюме");
+        System.out.println("Пытаемся удалить несуществующее резюме uuid42");
         ARRAY_STORAGE.delete("uuid42");
 
-        System.out.println("Делаем update");
+        System.out.println("Делаем update " + r1.getUuid());
         ARRAY_STORAGE.update(r1);
 
-        System.out.println("Удаляем резюме");
+        System.out.println("Удаляем резюме " + r1.getUuid());
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Очищаем массив");
+        System.out.println("Удаляем резюме " + r3.getUuid());
+        ARRAY_STORAGE.delete(r3.getUuid());
+        printAll();
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        System.out.println("Очищаем массив резюме");
         ARRAY_STORAGE.clear();
         printAll();
         System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
-        //System.out.println("\nGet All");
+        System.out.print("Print All:\t");
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.print(r + "\t");
         }
