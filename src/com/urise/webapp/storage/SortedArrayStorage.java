baa81ad9dbc94c2faf05760d62d0522070e48217
MyName Ok <gh.pueblo@gm.com>
2020-06-10 18:51:12 +0300

    HW3 rel07: modified: getIndex() - now use Arrays.binarySearch() instead of self-made binary search; insert() - now uses "index" from superclass, some operations moved to template method; remove() - changed bound of copying, some operations moved to template method.

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -2,46 +2,24 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public class SortedArrayStorage extends AbstractArrayStorage {
-    private int insertIndex;
 
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-        /*Далее реализован двоичный поиск по аналогии с методом Arrays.binarySearch()
-         * Однако, есть нюанс. При сравнении строк "uuid8" оказывается больше, чем "uuid23"
-         * Возможно из-за отсутствия в коде записи Comparable midVal = (Comparable)a[mid]
-         * По словам Татьяны Arrays.binarySearch() дает лексикографическое сравнение строк
-         * и такой проблемы там нет.*/
-        int low = 0, high = size - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int cmp = searchKey.compareTo(storage[mid]);
-            if (cmp > 0)
-                low = mid + 1;
-            else if (cmp < 0)
-                high = mid - 1;
-            else
-                return mid; // key found
-        }
-        insertIndex = low; // storage[insertIndex] - точка вставки резюме
-        return -(low + 1);  // key not found.
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
     protected void insert(Resume r) {
-        for (int i = size; i >= insertIndex; i--) {
-            storage[i + 1] = storage[i];
-            // проверка на возможное переполнение произведена в save().
-        }
-        // IDEA предложила альтернативу, но она вообще не дружественная
-        // System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size + 1 - insertIndex);
-
-        storage[insertIndex] = r;
-        size++;
+        // проверка на возможное переполнение произведена в save().
+        index = -(index + 1); // точка вставки резюме
+        System.arraycopy(storage, index, storage, index + 1, size + 1 - index);
+        storage[index] = r;
     }
 
-    protected void extract(int index) {
-        System.arraycopy(storage, index + 1, storage, index, size);
-        size--;
+    protected void remove(int index) {
+        System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
     }
 }
