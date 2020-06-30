f1431e70dd3f9bf3bf833630af22f577a801cb1f
MyName Ok <gh.pueblo@gm.com>
2020-06-30 16:10:56 +0300

    HW4 rel06: Рефлексия.

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,12 +3,14 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume("uuid1111");
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class<? extends Resume> resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println("Поле называется: " + field.getName());
 
@@ -21,22 +23,25 @@ public class MainReflection {
         System.out.println("Заменили поле uuid значением: " + r);
 
         // выводим все поля класса
-        Field[] fields = r.getClass().getDeclaredFields();
-        for(Field i : fields) {
+        Field[] fields = resumeClass.getDeclaredFields();
+        for (Field i : fields) {
             System.out.println("Список всех полей класса: " + i);
         }
 
         // выводим все методы класса
         // Idea заставляет предупредить о NoSuchMethodException
-        Method[] methods = r.getClass().getDeclaredMethods();
-        for(Method i : methods) {
+        Method[] methods = resumeClass.getDeclaredMethods();
+        for (Method i : methods) {
             System.out.println("Список всех методов класса: " + i);
         }
 
         // выводим метод toString()
-        Method method = r.getClass().getDeclaredMethod("toString");
-        System.out.println("method toString() " + method);
-
+        System.out.println("method toString() " + resumeClass.getDeclaredMethod("toString"));
         System.out.println("method toString() " + methods[1]);
+
+        // Реализуем обращение к методу toString() нашего объекта  согласно разбору ДЗ
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(r);
+        System.out.println(result);
     }
 }
