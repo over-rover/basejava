9220d3dccba66d0b8183da0315b920de17594f43
MyName Ok <gh.pueblo@gm.com>
2020-08-13 19:59:32 +0300

    Лекция 6. Реализовали поиск binarySearh с помощью вложенного (или локального? пока не уяснил) класса ResumeComparator. Проблема в том, что начал валиться тест getAll, - причину не понимаю.

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,13 +3,21 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected Object getKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, new ResumeComparator());
+    }
+
+    private static class ResumeComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
     }
 
     @Override
